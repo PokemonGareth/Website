c0fef4a53b0388cd1c2f728e70fd10025d2ee520
Gareth Searle <SEA19004582@btc.ac.uk>
2024-05-28 16:33:50 +0100

    Added navigation methods, and tweaked some page names

---
+++ Broadleigh/ProductSearch.php
@@ -0,0 +1,2 @@
+<!--This is for the searching page. Here is where the user will be taken when the press the search button on the products page. from here it will display the products in a list (similar to the editing pages) there will be a filter button and from here it will limit down the items visable.-->
+
--- Broadleigh/inc/header.php
+++ Broadleigh/inc/header.php
@@ -12,19 +12,22 @@
 
   <nav class="navbar navbar-expand-lg navbar-light bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="./index.php">Online Shop</a>
+    <a class="navbar-brand" href="./index.php">Broadleigh Gardens</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-
-      <li class="nav-item">
+        <li class="nav-item">
+          <a class="nav-link" href="./product.php">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./member.php">Members</a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link" href="./login.php"><i class="bi bi-person-circle" style="font-size: 2rem"></i></a>
-      </li>
+        </li>
       </ul>
-
-      
     </div>
   </div>
 </nav>
